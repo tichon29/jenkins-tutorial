f14f04847efcbf96532da965e243c318a0ea575a
Stephane Hervochon <stephane.hervochon@genesys.com>
2023-05-25 18:39:29 +0200

    Add js script

---
+++ firstJenkinsProject/checkfile.js
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process');
+
+const commitIdOnMasterBranch = execSync(' git hist | grep origin/main | awk \'{print $2}\'').toString().trim();
+const listOfUpdatedFiles = execSync(`git diff --name-only ${commitIdOnMasterBranch}..HEAD`).toString().trim().split('\n');
+let updatedMetadata = false;
+for (let index = 0; index < listOfUpdatedFiles.length && !updatedMetadata; index++) {
+    console.log('TTES');
+    if (listOfUpdatedFiles[index] && listOfUpdatedFiles[index].indexOf('README2') !== -1) {
+        updatedMetadata = true;
+    }
+}
+
+console.log(updatedMetadata);
+
