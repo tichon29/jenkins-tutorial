0be91c9ffe48c8a0a15916584a98738f8b109fb7
Stephane Hervochon <stephane.hervochon@genesys.com>
2023-05-26 09:02:47 +0200

    add logs in checkfile.js

--- firstJenkinsProject/checkfile.js
+++ firstJenkinsProject/checkfile.js
@@ -1,15 +1,20 @@
 const { execSync } = require('child_process');
 
 const commitIdOnMasterBranch = execSync(' git hist | grep origin/main | awk \'{print $2}\'').toString().trim();
+console.log(commitIdOnMasterBranch);
 const listOfUpdatedFiles = execSync(`git diff --name-only ${commitIdOnMasterBranch}..HEAD`).toString().trim().split('\n');
+console.log(listOfUpdatedFiles);
 let updatedMetadata = false;
 for (let index = 0; index < listOfUpdatedFiles.length && !updatedMetadata; index++) {
+    console.log(listOfUpdatedFiles[index]);
     if (listOfUpdatedFiles[index] && listOfUpdatedFiles[index].indexOf('README') !== -1) {
         updatedMetadata = true;
     }
 }
-
+console.log('All files reviewed');
 if (updatedMetadata) {
     console.log(updatedMetadata);
+} else {
+    console.log('File not updated');
 }
 
